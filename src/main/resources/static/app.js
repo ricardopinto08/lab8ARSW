c1de26ecd6a8984ebe953099dc611408b0cc4771
Ricardo Pinto <ricardo.pinto@mail.escuelaing.edu.co>
2017-03-16 09:44:50 -0500

    PARTE 1.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,16 +1,21 @@
 var stompClient = null;
+var punto = null;
 
 function connect() {
     var socket = new SockJS('/stompendpoint');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        
-        stompClient.subscribe('/topic/TOPICOXX', function (data) {
-           
-            
+        stompClient.subscribe('/topic/newpoint', function (data) {
+            var theObject=JSON.parse(data.body);
+            alert(theObject.x+" "+ theObject.y);
         });
     });
+}
+
+function sendPoint(x,y){
+    punto = {x:x.value,y:y.value};
+    stompClient.send("/topic/newpoint", {}, JSON.stringify(punto));
 }
 
 function disconnect() {
